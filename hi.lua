05d65de60f2919b6d5f1119fecb306dd8add49ea
fork <134505813+uhhfork@users.noreply.github.com>
2025-08-02 06:38:52 +0700

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -1,1 +1,12 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
+local games = {
+    [93557410403539] = 'https://raw.github.com/uhhfork/nebula/main/games/nmd.lua',
+    [104965156633249] = 'https://raw.github.com/uhhfork/nebula/main/games/poop.lua',
+}
+
+if games[game.PlaceId] then
+    loadstring(game:HttpGet(games[game.PlaceId]))()
+end
