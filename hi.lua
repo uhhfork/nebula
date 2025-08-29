e252739bbee1fca0a722d54e668e7a09b7019daa
fork <134505813+uhhfork@users.noreply.github.com>
2025-08-29 10:23:04 +0700

    uh

--- hi.lua
+++ hi.lua
@@ -2,13 +2,11 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local rhf = 'https://raw.githubusercontent.com/uhhfork/nebula/refs/heads/main/games/Protected_7711649348601202.lua.txt'
-
 local games = {
     [93557410403539] = 'https://raw.github.com/uhhfork/nebula/main/games/nmd.lua',
     [104965156633249] = 'https://raw.github.com/uhhfork/nebula/main/games/poop.lua',
-    [135406051460913] = rhf,
-    [103536205513886] = rhf
+    [135406051460913] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
+    [103536205513886] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua'
 }
 
 if games[game.PlaceId] then
