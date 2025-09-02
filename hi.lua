dde94c0f4e4a0b0cd73be3ed3524607353735489
Imfcatfr <tyoimoir@gmail.com>
2025-09-02 14:36:11 +0100

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -6,7 +6,8 @@ local games = {
     [93557410403539] = 'https://raw.github.com/uhhfork/nebula/main/games/nmd.lua',
     [104965156633249] = 'https://raw.github.com/uhhfork/nebula/main/games/poop.lua',
     [135406051460913] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
-    [103536205513886] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua'
+    [103536205513886] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
+    [126509999114328] = 'https://raw.github.com/uhhfork/nebula/main/games/forest.lua'
 }
 
 if games[game.PlaceId] then
