74edce0160cf0063ad6ed8658506adfe90132e5a
fork <134505813+uhhfork@users.noreply.github.com>
2025-09-07 13:38:40 +0700

    Update rhf.lua

--- games/rhf.lua
+++ games/rhf.lua
@@ -1,1 +1,1 @@
-Instance.new("Message", workspace).Text = "run hide fight currently not working right now. for more information, join our discord server: https://dsc.gg/rigbyfanclub"
+Instance.new("Message", workspace).Text = "run hide fight script currently not working right now. for more information, join our discord server: https://dsc.gg/rigbyfanclub"
