d429a0ba2cdb6fb6475156965c1968cb91503441
fork <134505813+uhhfork@users.noreply.github.com>
2025-09-07 15:32:47 +0700

    Update rhf.lua

--- games/rhf.lua
+++ games/rhf.lua
@@ -1,8 +1,7 @@
 loadstring(game:HttpGet("https://raw.github.com/uhhfork/nebula/main/stuff/a.lua"))()
 
 kick([[
-Run Hide Fight script is currently not working.
+Run Hide Fight script is currently down on purpose.
 For more information please join Nebula Discord Server.
-
 <font color="#5865F2"><u>https://dsc.gg/rigbyfanclub</u></font>
 ]])
