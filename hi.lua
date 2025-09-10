87ca8a640615e655a41c7d8f0d8112d935016bbf
fork <134505813+uhhfork@users.noreply.github.com>
2025-09-10 17:58:43 +0700

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -1,10 +1,10 @@
+loadstring(game:HttpGet("https://raw.github.com/uhhfork/nebula/main/stuff/a.lua
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
 local games = {
     [93557410403539] = 'https://raw.github.com/uhhfork/nebula/main/games/nmd.lua',
-    [104965156633249] = 'https://raw.github.com/uhhfork/nebula/main/games/poop.lua',
     [135406051460913] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
     [103536205513886] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
     [126509999114328] = 'https://raw.github.com/uhhfork/nebula/main/games/forest.lua'
@@ -12,4 +12,6 @@ local games = {
 
 if games[game.PlaceId] then
     loadstring(game:HttpGet(games[game.PlaceId]))()
+    else
+    kick("Unsupported Game")
 end
