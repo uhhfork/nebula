5662d0308bbef1dce9f54efd77cb641b7750c0b5
Imfcatfr <tyoimoir@gmail.com>
2025-09-12 01:27:45 +0100

    six and seven

--- games/rhf.lua
+++
@@ -1,7 +0,0 @@
-loadstring(game:HttpGet("https://raw.github.com/uhhfork/nebula/main/stuff/a.lua"))()
-
-kick([[
-Run Hide Fight script is currently down on purpose.
-For more information please join Nebula Discord Server.
-<font color="#5865F2"><u>discord.gg/nebulahub</u></font>
-]])
