e900cdc6e684effcf556db8c6ae9cc3aad290c98
fork <134505813+uhhfork@users.noreply.github.com>
2025-09-18 09:24:34 +0700

    fuck u github

--- hi.lua
+++ hi.lua
@@ -4,7 +4,7 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-loadstring(game:HttpGet("https://github.com/uhhfork/nebula/raw/main/stuff/a.lua"))()
+loadstring(game:HttpGet('https://github.com/uhhfork/nebula/raw/main/stuff/a.lua'))()
 
 local games = {
     [93557410403539] = 'https://github.com/uhhfork/nebula/raw/main/games/nmd.lua',
