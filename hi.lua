25ba0d0953733c1a2105f8b2fa17edf481e121a0
fork <134505813+uhhfork@users.noreply.github.com>
2025-09-18 09:23:56 +0700

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -4,13 +4,13 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-loadstring(game:HttpGet("https://raw.github.com/uhhfork/nebula/main/stuff/a.lua"))()
+loadstring(game:HttpGet("https://github.com/uhhfork/nebula/raw/main/stuff/a.lua"))()
 
 local games = {
-    [93557410403539] = 'https://raw.github.com/uhhfork/nebula/main/games/nmd.lua',
-    [135406051460913] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
-    [103536205513886] = 'https://raw.github.com/uhhfork/nebula/main/games/rhf.lua',
-    [126509999114328] = 'https://raw.github.com/uhhfork/nebula/main/games/forest.lua'
+    [93557410403539] = 'https://github.com/uhhfork/nebula/raw/main/games/nmd.lua',
+    [135406051460913] = 'https://github.com/uhhfork/nebula/raw/main/games/rhf.lua',
+    [103536205513886] = 'https://github.com/uhhfork/nebula/raw/main/games/rhf.lua',
+    [126509999114328] = 'https://github.com/uhhfork/nebula/raw/main/games/forest.lua'
 }
 
 if games[game.PlaceId] then
