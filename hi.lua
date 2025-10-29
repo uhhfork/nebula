37bea6d828c762f15de22c00f86a77bb2aabe88a
Imfcatfr <tyoimoir@gmail.com>
2025-10-30 00:00:40 +0100

    Update game URLs to use new API endpoint

--- hi.lua
+++ hi.lua
@@ -7,13 +7,13 @@ end
 loadstring(game:HttpGet('https://github.com/uhhfork/nebula/raw/main/stuff/a.lua'))()
 
 local games = {
-    [8355209732] = 'https://github.com/uhhfork/nebula/raw/main/games/rhf.lua',
-    [7326934954] = 'https://github.com/uhhfork/nebula/raw/main/games/forest.lua',
-    [4914269443] = 'https://github.com/uhhfork/nebula/raw/main/games/uns.lua'
+    [8355209732] = 'https://api.getnebula.lol/games/rhf.lua',
+    [7326934954] = 'https://api.getnebula.lol/games/forest.lua',
+    [4914269443] = 'https://api.getnebula.lol/games/uns.lua'
 }
 
 if games[game.GameId] then
     loadstring(game:HttpGet(games[game.GameId]))()
-    else
+else
     kick('Unsupported Game')
 end
