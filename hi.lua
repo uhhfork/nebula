4da93fd29e22d516799a86697cb5cb827d272c87
fork <134505813+uhhfork@users.noreply.github.com>
2025-11-06 06:49:25 +0700

    yea

--- hi.lua
+++ hi.lua
@@ -1,19 +1,8 @@
--- hi
-
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
 loadstring(game:HttpGet('https://github.com/uhhfork/nebula/raw/main/stuff/a.lua'))()
 
-local games = {
-    [8355209732] = 'https://github.com/uhhfork/nebula/raw/main/games/rhf.lua',
-    [7326934954] = 'https://github.com/uhhfork/nebula/raw/main/games/forest.lua',
-    [4914269443] = 'https://github.com/uhhfork/nebula/raw/main/games/uns.lua'
-}
-
-if games[game.GameId] then
-    loadstring(game:HttpGet(games[game.GameId]))()
-    else
-    kick('Unsupported Game')
-end
+kick([[Loader changed. Join discord.gg/MPzuWvg2BT for new loader.
+Btw goodluck skidders from tryna steal my work and make em yours, cuz yea yall will get this kick message anyways.]])
